382cd8ae9323c289311f614e31aec1b8f207b8e6
saurabhbassi <avishekdas128@gmail.com>
2020-07-29 21:41:07 +0530

    SECOND COMMIT

--- app/src/main/java/com/orange/testgit/MainActivity.java
+++ app/src/main/java/com/orange/testgit/MainActivity.java
@@ -3,12 +3,17 @@ package com.orange.testgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.hello_world);
+        textView.setText("THIS IS A CHANGE");
     }
 }
